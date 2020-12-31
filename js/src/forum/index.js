856bae83737741409355a1faf149afeae99d2174
Yixuan Qiu <yixuanq@gmail.com>
2020-12-31 19:25:57 +0800

    also render math expressions on post updates

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,8 +3,32 @@ import CommentPost from 'flarum/components/CommentPost';
 import TextEditor from 'flarum/components/TextEditor';
 
 app.initializers.add('cosname-katex', function() {
-  // render math expression on every post loading
+  // render math expressions on every post loading
   extend(CommentPost.prototype, 'oncreate', function() {
+
+    // remove the <code> tags that enclose KaTeX
+    // (we added <code class="katex-escape"> tags on LaTeX expressions when saving the post)
+    var katex_expr = $('code.katex-escape', this.element);
+    katex_expr.each(function() {
+      $(this).replaceWith($(this).html());
+    });
+
+    // run KaTeX renderer on the single post body (not on the entire page or the entire post)
+    renderMathInElement($('div.Post-body', this.element)[0], {
+      // do not render inside those tags
+      "ignoredTags":["script", "noscript", "style", "textarea", "pre", "code"],
+      // those are the delimiters we are going to use to write latex formulas
+      "delimiters":[
+        {left: "$$",  right: "$$",  display: true},
+        {left: "\\[", right: "\\]", display: true},
+        {left: "\\(", right: "\\)", display: false}
+      ]
+    });
+
+  });
+
+  // also render math expressions on post updates, e.g., after editing the content
+  extend(CommentPost.prototype, 'onupdate', function() {
 
     // remove the <code> tags that enclose KaTeX
     // (we added <code class="katex-escape"> tags on LaTeX expressions when saving the post)
